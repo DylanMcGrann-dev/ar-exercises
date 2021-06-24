b6fcdd0d4987b06bc694f35b2b683ed0c67f0bef
Dylan McGrann <dylanmcgrann@Dylans-MacBook-Pro.local>
2021-06-23 18:34:56 -0600

    user can name a new store, error message displayed if store doesnt meet validations

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Darth", last_name: "vader", hourly_rate: 55)
+@store1.employees.create(first_name: "King", last_name: "Mcgrann", hourly_rate: 45)
+
+@store2.employees.create(first_name: "Rick", last_name: "Sanches", hourly_rate: 60)
+@store2.employees.create(first_name: "Deku", last_name: "Hero", hourly_rate: 55)
+@store2.employees.create(first_name: "Eren", last_name: "Yager", hourly_rate: 45)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates_numericality_of :hourly_rate, :greater_than => 40
+  validates_numericality_of :hourly_rate, :less_than => 200
+  validates :store, presence: true
+end
+class Store < ActiveRecord::Base
+  validates :annual_revenue, numericality: { only_integer: true }
+  validates_numericality_of :annual_revenue, :greater_than => 0
+  validates_length_of :name, minimum: 3
+end
+puts "enter a store name"
+@input = $stdin.gets.chomp
+@store3 = Store.create(name: @input)
+puts @store3.errors.full_messages
